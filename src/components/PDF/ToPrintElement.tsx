12591c5a7ca15f97b7fea335bf890ff4faf21a75
Mikoyzskie <rapidstrike13@gmail.com>
2024-08-07 10:36:19 +0800

    chore: + label background

--- src/components/PDF/ToPrintElement.tsx
+++ src/components/PDF/ToPrintElement.tsx
@@ -8,28 +8,30 @@ export default function ToPrintElement({ boards }: { boards: DisplayBoard }) {
             {boards &&
                 boards.Board_Items.map((item: BoardItem, index: number) => {
                     return (
-                        <div key={index} className="w-full h-[184px] bg-white rounded-[6px] text-black p-4 flex items-center gap-6">
-                            <div className="grow flex flex-col h-full">
-                                <span className="text-base font-bold uppercase">{item.Board_Items.Parent_Product ? item.Board_Items.Parent_Product.Name : item.Board_Items.Code}</span>
-                                <br />
-                                <p className="text-[11px]">{item.Board_Items.Code}</p>
+                        <div key={index} className="board-item">
+                            <div className=" w-full h-[184px] bg-[rgba(255,255,255,85%)] rounded-[6px] text-black p-4 flex items-center gap-6">
+                                <div className="grow flex flex-col h-full ">
+                                    <span className="text-base font-bold uppercase">{item.Board_Items.Parent_Product ? item.Board_Items.Parent_Product.Name : item.Board_Items.Code}</span>
+                                    <br />
+                                    <p className="text-[11px]">{item.Board_Items.Code}</p>
 
-                                {
-                                    item.Board_Items.Description ? <p className="text-[11px]">{item.Board_Items.Description}</p> : ""
-                                }
-                                {
-                                    item.Board_Items.Item_Finish ? <p className="text-[11px]">Finish: {item.Board_Items.Item_Finish.Finish_ID}</p> : ""
-                                }
-                                {
-                                    item.Board_Items.Item_Function ? <p className="text-[11px]">Function: {item.Board_Items.Item_Function.Function}</p> : ""
-                                }
-                            </div>
-                            <div className="w-[100px] flex flex-col items-center justify-center gap-3">
-                                <QRCode
-                                    value="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                                    size={100}
-                                />
-                                <span className="text-xs break-words max-w-[100px]">hhttps://cleanuri.com/qX8MyN</span>
+                                    {
+                                        item.Board_Items.Description ? <p className="text-[11px]">{item.Board_Items.Description}</p> : ""
+                                    }
+                                    {
+                                        item.Board_Items.Item_Finish ? <p className="text-[11px]">Finish: {item.Board_Items.Item_Finish.Finish_ID}</p> : ""
+                                    }
+                                    {
+                                        item.Board_Items.Item_Function ? <p className="text-[11px]">Function: {item.Board_Items.Item_Function.Function}</p> : ""
+                                    }
+                                </div>
+                                <div className="w-[100px] flex flex-col items-center justify-center gap-3">
+                                    <QRCode
+                                        value="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+                                        size={100}
+                                    />
+                                    <span className="text-xs break-words max-w-[100px]">hhttps://cleanuri.com/qX8MyN</span>
+                                </div>
                             </div>
                         </div>
                     )
